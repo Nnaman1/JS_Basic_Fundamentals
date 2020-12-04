c1e63edd7f0988de013197209cff8771d9511ed9
Nnaemeka <N.nnaemeka@gmail.com>
2020-12-04 14:44:39 -0600

    more Operator prac

--- prac.js
+++ prac.js
@@ -1,7 +1,17 @@
-//Basic Operator 
+//Math Operator 
 const year = 2050;
 const joeAge = (year - 1990);
 const billAge = (year - 2020);
 
 console.log(joeAge, billAge)
 console.log(joeAge * 2, billAge / 10, 2 ** 3);
+
+//Assignment Operator
+let x = 2;
+x = + 22;
+
+console.log(x);
+
+//Comparison Operator
+
+console.log(billAge > joeAge);
