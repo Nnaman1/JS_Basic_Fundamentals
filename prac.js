714b31124220c344a8eed149d72ae3ac95b42ff7
Nnaemeka <N.nnaemeka@gmail.com>
2020-12-04 14:23:53 -0600

    basic Operator prac

--- prac.js
+++ prac.js
@@ -1,9 +1,7 @@
-//Mutate
-let name = `Nnaemeka`;
-name = `Joe`;
-console.log(name);
+//Basic Operator 
+const year = 2050;
+const joeAge = (year - 1990);
+const billAge = (year - 2020);
 
-//Non-Mutate
-const dob = 1999;
-console.log(dob);
-
+console.log(joeAge, billAge)
+console.log(joeAge * 2, billAge / 10, 2 ** 3);
