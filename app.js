1fce281f7bf4aec7a943e8792c6f2c66726400fb
Nnaemeka <N.nnaemeka@gmail.com>
2020-12-06 15:48:54 -0600

    worked on frist few prob

--- app.js
+++ app.js
@@ -2,32 +2,28 @@
 
 //Create a variable called myName that is a string data type
 
-//Code here
+let myName = 'Nnaemeka Nnamani';
 
 //////////////////PROBLEM 2////////////////////
 
 //Create a variable called myAge that is a number data type
 
-//Code here
-
+let myName = 28;
 //////////////////PROBLEM 3////////////////////
 
 //Create a variable called lovesCode that is a boolean data type
 
-//Code here
-
+let lovesCode = true;
 //////////////////PROBLEM 4////////////////////
 
 //Create a variable called greatestFear that is undefined because we fear nothing
 
-//Code here
-
+let greatestFear;
 //////////////////PROBLEM 5////////////////////
 
 //Create a variable called devMountainGoal that is null because we are just starting out
 
-//Code here
-
+let devMountainGoal = null;
 //////////////////PROBLEM 6////////////////////
 
 //Create a function declaration called greeting that
