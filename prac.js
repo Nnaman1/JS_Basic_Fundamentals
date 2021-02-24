af2443088826b8263014824b34c8c0ef69f649d8
Nnaemeka <N.nnaemeka@gmail.com>
2021-02-24 14:17:20 -0600

    boolen prac

--- prac.js
+++ prac.js
@@ -1,19 +1,13 @@
-// Notes
-
-const birthYear = 1998;
+let birthYear = 2022;
 
 let century;
 
 if (birthYear >= 2000){
-    console.log('Welcome to the 21st century!')
-} else {
-    console.log('Welcome to the 20th century!')
+    century = 21;
+
+}
+else {
+    century = 20;
 }
 
-let money ;
-
-if (money){
-    console.log('Save your money')
-} else {
-    console.log('get a job!')
-}
+console.log(century);
